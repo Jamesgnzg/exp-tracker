bae9e80c25213cf05ea9718e6a38d554f02525f7
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-06-24 22:36:08 +0800

    Add transaction layout; Search button sample card

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
  
  <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
 	<div class="h-full px-3 py-4 pl-10 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-	   <a href="https://flowbite.com/" class="flex items-center ps-2.5 mb-5">
+	   <a href="https://flowbite.com/" class="flex items-center ps-2.5 mb-20">
 		  <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Tracker</span>
 	   </a>
 	   <ul class="space-y-2 font-medium">
@@ -82,6 +82,6 @@
 	</div>
  </aside>
  
- <div class="p-4 sm:ml-64">
+ <main class="p-4 sm:ml-64">
 	{@render children()}
- </div>
+ </main>
--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,1 +1,23 @@
-<h1 class="overline">Transactions</h1>
+<div class="flex flex-col md:flex-row">
+    <div class="p-2 md:w-4/5">
+        <form class="max-w-md mx-auto">   
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border-b-1 border-0 border-gray-300 focus:ring-gray-500 focus:border-gray-100 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+            </div>
+        </form>
+    </div>
+    <div class="p-2 md:w-2/5">
+        <div>
+            <a href="/transaction" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            </a>
+        </div>
+    </div>
+</div>
