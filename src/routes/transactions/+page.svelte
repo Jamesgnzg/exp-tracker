ab9f7751379295263ffe803e8cecb23bfd13300f
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-06-25 21:34:59 +0800

    Add user card layout

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -11,13 +11,15 @@
                 <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border-b-2 border-0 border-gray-200 transition duration-300 delay-200 ease focus:outline-none focus:ring-0 focus:border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
             </div>
         </form>
+
     </div>
     <div class="p-2 md:w-2/5">
-        <div>
-            <a href="/transaction" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </a>
+        <div class="flex max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
+            <img class="w-15 h-15 rounded-full mr-4" alt="Avatar">
+            <div class="text-md">
+                <p>Jonathan Kuminga</p>
+                <p>Shooting Guard</p>
+            </div>
         </div>
     </div>
 </div>
