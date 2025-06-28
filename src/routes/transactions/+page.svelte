97a47066ae91f9aa3b141ac24d1968d10b3e9383
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-06-28 21:18:52 +0800

    Add categories placeholder

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,17 +1,36 @@
 <div class="flex flex-col md:flex-row">
     <div class="p-2 md:w-4/5">
         <form class="max-w-md mx-auto">   
-            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <label for="default-search" class="absolute mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
             <div class="relative">
                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
-                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border-b-2 border-0 border-gray-200 transition duration-300 delay-200 ease focus:outline-none focus:ring-0 focus:border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
+                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border-b-2 border-0 border-gray-200 transition duration-300 delay-200 ease focus:outline-none focus:ring-0 focus:border-yellow-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" autocomplete="off" placeholder="Search"/>
             </div>
         </form>
 
+        <div class="md:mt-8">
+            <p>Categories</p>
+            <div class="flex p-5 gap-3">
+                <div class="flex max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
+                    <img class="w-15 h-15 rounded-full mr-4" src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="Avatar">
+                    <div class="text-md">
+                        <p>Jonathan Kuminga</p>
+                        <p>Shooting Guard</p>
+                    </div>
+                </div>
+                <div class="flex max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
+                    <img class="w-15 h-15 rounded-full mr-4" src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="Avatar">
+                    <div class="text-md">
+                        <p>Jonathan Kuminga</p>
+                        <p>Shooting Guard</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="p-2 md:w-2/5">
         <div class="flex max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
