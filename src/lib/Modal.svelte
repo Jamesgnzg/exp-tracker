7940ae365ec8a04f666e0f7061c6eed2f953f505
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-07-22 15:30:56 +0800

    Update modal styling

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -6,22 +6,25 @@
     let dialog = $state();
 
     $effect(() => {
-        if (showModal) dialog.showModal();
+        if (showModal) {
+            dialog.showModal();
+        } else {
+            dialog.close();
+        }
     })
 </script>
 
 <dialog bind:this={dialog}
     onclose={() => showModal = false}
-    data-modal-backdrop="static"
-    class="overflow-y-auto overflow-x-hidden rounded-md absolute left-1/2 top-1/3 transform -translate-1/2 min-w-2xl min-h-2xl"
-    transition:fade>
-    
+    class="overflow-y-auto overflow-x-hidden rounded-md absolute left-1/2 top-1/3 transform -translate-1/2 min-w-2xl min-h-2xl backdrop:bg-black/80"
+    >
+
     {#if showModal}
         <div class="p-5" transition:fade>
-            <div class="flex justify-between border-b pb-3">
+            <div class="flex justify-between border-b border-b-gray-200 pb-3">
                 {@render header?.()}
                 <!-- svelte-ignore a11y_autofocus -->
-                <button autofocus onclick={() => dialog.close()} class="cursor-pointer"><X /></button>
+                <button autofocus onclick={() => dialog.close()} class="cursor-pointer text-gray-500 hover:bg-gray-200 rounded-lg p-1.5"><X /></button>
             </div>
             {@render children?.()}
         </div>
--- src/routes/transactions/components/RightBar.svelte
+++ src/routes/transactions/components/RightBar.svelte
@@ -17,27 +17,27 @@
 
 <Modal bind:showModal={showTransactionModal}>
 	{#snippet header()}
-		<h2 class="font-bold">Create Transaction</h2>
+		<h2 class="text-xl font-medium text-gray-900">Create Transaction</h2>
 	{/snippet}
 
     <div class="mt-2">
         <form method="POST">
             <div class="grid grid-cols-2 gap-2">
                 <div class="col-span-2">
-                    <label for="name" class="block mb-2 font-medium">
+                    <label for="name" class="text-sm rtl:text-right font-medium block text-gray-700 dark:text-gray-200 space-y-2 mb-1">
                         Transaction
                     </label>
-                    <input name="name" class="border p-2 rounded-sm min-w-full focus:ring-0 focus:outline-none" autocomplete="off" placeholder="Add Transaction name"/>
+                    <input name="name" class="block w-full disabled:cursor-not-allowed disabled:opacity-50 rtl:text-right focus:outline-hidden text-sm px-2.5 py-2.5 border border-gray-300 focus:border-primary-500 focus:ring-orange-500 bg-gray-50 text-gray-900 rounded-lg" required autocomplete="off" placeholder="Add Transaction name"/>
                 </div>
                 <div class="col-span-2 sm:col-span-1">
-                    <label for="amount" class="block mb-2 font-medium">
+                    <label for="amount" class="text-sm rtl:text-right font-medium block text-gray-700 dark:text-gray-200 space-y-2 mb-1">
                         Amount
                     </label>
-                    <input name="amount" type="number" class="border p-2 rounded-sm min-w-full focus:ring-0 focus:outline-none" autocomplete="off" placeholder="How much did it cost?"/>
+                    <input name="amount" type="number" class="block w-full disabled:cursor-not-allowed disabled:opacity-50 rtl:text-right focus:outline-hidden text-sm px-2.5 py-2.5 border border-gray-300 focus:border-primary-500 focus:ring-orange-500 bg-gray-50 text-gray-900 rounded-lg" autocomplete="off" placeholder="How much did it cost?"/>
                 </div>
                 <div class="col-span-2 sm:col-span-1">
-                    <label for="category" class="block mb-2 font-medium text-gray-900 dark:text-white">Category</label>
-                    <select id="category" class="border rounded-sm focus:outline-none focus:ring-0 min-w-full p-2">
+                    <label for="category" class="text-sm rtl:text-right font-medium block text-gray-700 dark:text-gray-200 space-y-2 mb-1">Category</label>
+                    <select id="category" class="block w-full disabled:cursor-not-allowed disabled:opacity-50 rtl:text-right focus:outline-hidden text-sm px-2.5 py-2.5 border border-gray-300 focus:border-primary-500 focus:ring-orange-500 bg-gray-50 text-gray-900 rounded-lg">
                         <option selected={true}>Select category</option>
                         <option value="TV">Transportation</option>
                         <option value="PC">Food</option>
@@ -46,15 +46,21 @@
                     </select>
                 </div>
                 <div class="col-span-2">
-                    <label for="description" class="block mb-2 font-medium">
+                    <label for="description" class="text-sm rtl:text-right font-medium block text-gray-700 dark:text-gray-200 space-y-5 mb-1">
                         Description
                     </label>
-                    <textarea name="description" class="border p-2 rounded-sm min-w-full focus:ring-0 focus:outline-none" autocomplete="off" placeholder="Details of the expense"></textarea>
+                    <textarea name="description" class="block w-full disabled:cursor-not-allowed disabled:opacity-50 rtl:text-right focus:outline-hidden text-sm px-2.5 py-2.5 border border-gray-300 focus:border-primary-500 focus:ring-orange-500 bg-gray-50 text-gray-900 rounded-lg" autocomplete="off" placeholder="Details of the expense"></textarea>
                 </div>
             </div>
-            <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
-                Add new Transaction
-            </button>
+
+            <div class="flex flex-row-reverse gap-2">
+                <button class="text-gray-900 inline-flex items-center bg-transparent border border-gray-200 hover:bg-gray-100 hover:text-orange-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4 cursor-pointer" onclick={() => showTransactionModal = false}>
+                    Cancel
+                </button>
+                <button type="submit" class="text-white inline-flex items-center bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4 cursor-pointer">
+                    Add new Transaction
+                </button>
+            </div>
         </form>
     </div>
 </Modal>
